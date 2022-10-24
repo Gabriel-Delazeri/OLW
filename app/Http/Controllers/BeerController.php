e63131b261a88faacf901ddc9dd942cffcf57ed1
Gabriel-Delazeri <gbldelazeri@gmail.com>
2022-10-24 14:47:30 -0300

    feat: export sending email (before redis)

--- app/Http/Controllers/BeerController.php
+++ app/Http/Controllers/BeerController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Export;
+use App\Mail\ExportEmail;
 use App\Exports\BeerExport;
 use App\Services\PunkapiService;
 use App\Http\Requests\BeerRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 use Maatwebsite\Excel\Facades\Excel;
 
 class BeerController extends Controller
@@ -22,11 +26,21 @@ class BeerController extends Controller
             return collect($value)->only(['name', 'tagline', 'first_brewed', 'description']);
         })->toArray();
 
+        $filename = "cervejas-encontradas-". now()->format("Y-m-d - H_i") . ".xlsx";
+
         Excel::store
             (new BeerExport($filteredBeers),
-            'olw-report.xlsx',
+            $filename,
                 's3'
         );
+
+        Mail::to('gbldelazeridev@gmail.com')
+            ->send(new ExportEmail($filename));
+
+        Export::create([
+            'file_name' => $filename,
+            'user_id'   => Auth::user()->id
+        ]);
 
         return 'relatorio criado';
     }
